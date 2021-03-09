5200ccf6a7cc4a0ef7b3b6827b06715435d5ae93
Joonas Praks <joonaspraks@gmail.com>
2021-03-09 21:05:38 +0200

    Sorting by cap migrated to new backend folder

--- hw1/go-be/action/action.go
+++ hw1/go-be/action/action.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -74,7 +75,7 @@ func (r *Repository) DeleteActionWithID(ID string) {
 // genAllActions returns a json list of all actions in the internal action list
 func (r *Repository) GenAllActions() []byte {
 	buf := &bytes.Buffer{}
-	json.NewEncoder(buf).Encode(r.actions)
+	json.NewEncoder(buf).Encode(sortTodosByCaps(r.actions))
 	return buf.Bytes()
 }
 
@@ -87,4 +88,32 @@ func (r *Repository) UpdateActionwithID(ID string) []byte {
 		}
 	}
 	return buf.Bytes()
+}
+
+func checkStringAlphabet(str string) int {
+	a := 0
+	for _, charVariable := range str {
+		if charVariable >= 'A' && charVariable <= 'Z' {
+			a++
+		}
+	}
+	return a
+}
+
+func sortTodosByCaps(actions []*Action) []*Action {
+	sort.Slice(actions, func(i, j int) bool {
+		return checkStringAlphabet(actions[i].Message) > checkStringAlphabet(actions[j].Message)
+	})
+
+	for _, action := range actions {
+		fmt.Printf("%+v", action)
+	}
+
+	return actions
+}
+
+func testChecker() {
+	fmt.Println(checkStringAlphabet("ActioZ"))
+	fmt.Println(checkStringAlphabet("tegeVus"))
+	fmt.Println(checkStringAlphabet("HELLLOOO!!"))
 }
